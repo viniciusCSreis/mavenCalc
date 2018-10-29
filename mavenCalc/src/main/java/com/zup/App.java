cc84ffdeb8b9bb4eefc69a3d9801049f72ba7331
vinicius.sousa <vinicius.sousa@zup.com.br>
2018-10-29 17:49:25 -0300

    junit5 depends

--- mavenCalc/src/main/java/com/zup/App.java
+++ mavenCalc/src/main/java/com/zup/App.java
@@ -4,12 +4,22 @@ package com.zup;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args ) {
         Calc calc = new Calc();
         System.out.println( "1 + 2 ="+ calc.addition(1,2));
-        System.out.println( "1 + 2 ="+ calc.addition(-1,2));
-        System.out.println( "1 + 2 ="+ calc.addition(1,-2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        System.out.println( "1 + 2 ="+ calc.addition(1,2));
+        try{
+            System.out.println( "1 + 2 ="+ calc.addition(1,-2));
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
